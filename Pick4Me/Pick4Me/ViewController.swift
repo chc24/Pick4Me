e77c90d5fb06068e72cd41438bb06dd8e4ea3dcf
FezHacker <fezdcb@mgail.com>
2015-09-06 18:12:38 -0700

    Got Lat and Long

--- Pick4Me/Pick4Me/ViewController.swift
+++ Pick4Me/Pick4Me/ViewController.swift
@@ -12,7 +12,7 @@ import trnql
 let trnql = Trnql.sharedInstance // Optional: Store sharedInstance in a local variable
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         trnql.delegate = self
@@ -21,26 +21,39 @@ class ViewController: UIViewController {
         
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    func smartAddressChanged(addressEntry: AddressEntry?, error: NSError?) {
+        if error == nil {
+            var tmpArray = [String]() // Adding data to a tmp array then copying to tableView data source to avoid potential index out of bounds
+            
+            let latitude = addressEntry?.getLat() ?? ""
+            tmpArray.append("Latitude: \(latitude)")
+            
+            let longitude = addressEntry?.getLng() ?? ""
+            tmpArray.append("Longitude: \(longitude)")
+
+        } else {
+            println(error!)
+        }
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
 extension ViewController: TrnqlDelegate {
     
-    func smartActivityChanged(activityEntry: ActivityEntry?, error: NSError?) {
-        // do something with activityEntry, eg: activityEntry.isWalking()
-    }
-    
-    func smartAddressChanged(addressEntry: AddressEntry?, error: NSError?) {
-        // do something with addressEntry, eg: addressEntry.getAddress()
-    }
-    
-    func smartLocationChanged(locationEntry: LocationEntry?, error: NSError?) {
-        // do something with locationEntry, eg: locationEntry.getSpeed()
-    }
+//    func smartActivityChanged(activityEntry: ActivityEntry?, error: NSError?) {
+//        // do something with activityEntry, eg: activityEntry.isWalking()
+//    }
+//    
+//    func smartAddressChanged(addressEntry:AddressEntry?, error: NSError?) {
+//        // do something with addressEntry, eg: addressEntry.getAddress()
+//    }
+//    
+//    func smartLocationChanged(locationEntry: LocationEntry?, error: NSError?) {
+//        // do something with locationEntry, eg: locationEntry.getSpeed()
+//    }
 }
